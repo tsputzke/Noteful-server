60376a468289b088765cb1e6553043ca7c883bed
Timothy Putzke <tsputzke@gmail.com>
2019-08-17 15:05:12 -0400

    Add migrations and seed data

---
+++ seeds/seed.folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO folders (folder_name)
+VALUES
+  ('Folder A'),
+  ('Folder B'),
+  ('Folder C')
---
+++ seeds/seed.notes.sql
@@ -0,0 +1,7 @@
+INSERT INTO notes (note_name, folder_id, content)
+VALUES
+  ('Note 1', '1', 'content here'),
+  ('Note 2', '1', 'other content here'),
+  ('Note 3', '2', 'some content here'),
+  ('Note 4', '2', 'magic content here'),
+  ('Note 5', '3', 'fluffy content here')
